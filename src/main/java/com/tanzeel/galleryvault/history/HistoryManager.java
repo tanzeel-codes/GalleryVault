0ac14815e90a866a409ea176bf8d87bb8803a961
Tanzeel Akhtar <akhtartanzeel00@gmail.com>
2026-07-22 18:27:02 +0530

    Added persistent class to save history

--- src/main/java/com/tanzeel/galleryvault/history/HistoryManager.java
+++ src/main/java/com/tanzeel/galleryvault/history/HistoryManager.java
@@ -2,10 +2,17 @@ package com.tanzeel.galleryvault.history;
 
 public class HistoryManager {
     public void save(DownloadRecord record) {
-
-    }
-
-    public void getRecord() {
-
+        String line = String.format(
+                "%s,%s,%s, $%s%n",
+                record.getTimeStamp(),
+                record.getStatus(),
+                record.getUrl(),
+                record.getReason() == null ? "" : record.getReason()
+        );
+        try {
+            Files.writeString(HISTORY_FILE, line, StandardOpenOption.CREATE, StandardOpenOption.APPEND);
+        } catch (IOException e) {
+            throw new RuntimeException("Unable to save history.", e);
+        }
     }
 }
