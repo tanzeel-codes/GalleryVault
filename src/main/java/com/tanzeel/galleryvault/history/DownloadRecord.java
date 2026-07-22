2154c51c9884fd7f991dceba95fa69412ecaf0ae
Tanzeel Akhtar <akhtartanzeel00@gmail.com>
2026-07-22 18:28:03 +0530

    Added records detail POJO

--- src/main/java/com/tanzeel/galleryvault/history/DownloadRecord.java
+++ src/main/java/com/tanzeel/galleryvault/history/DownloadRecord.java
@@ -9,6 +9,22 @@ public class DownloadRecord {
     private String reason;
     private String platform;
 
-
-
+    public DownloadRecord(String url, LocalDateTime timestamp, DownloadStatus status, String reason) {
+        this.url = url;
+        this.timestamp = timestamp;
+        this.status = status;
+        this.reason = reason;
+    }
+    public String getUrl() {
+        return url;
+    }
+    public LocalDateTime getTimeStamp() {
+        return timestamp;
+    }
+    public DownloadStatus getStatus() {
+        return status;
+    }
+    public String getReason() {
+        return reason;
+    }
 }
